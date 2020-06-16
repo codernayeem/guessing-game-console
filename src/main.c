d2af81c6559118250ca37d81509994bdc2b38a6c
Md Nayeem <codernayeem898@gmail.com>
2020-06-16 22:52:36 +0600

    Added main.c file

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    printf("\n\t\t\t*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+    printf("\n\t\t\t*    Welcome to Guessing Game   *");
+    printf("\n\t\t\t*                               *");
+    printf("\n\t\t\t*\xB1\xB1\xB1\xB1 Made by  @codernayeem \xB1\xB1\xB1\xB1*");
+    printf("\n\t\t\t*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
+    printf("\n\nLet's play a game.\n\n");
+
+
+    getch();
+    return 0;
+}
